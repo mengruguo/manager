37fccf82fe450c7b48f0f846090e7b28ae695ff4
guomengru <guo.mengru@zte.com.cn>
2018-03-27 20:49:03 +0800

    app新增数据写入数据库实现，app查看界面优化

--- Manager/apks/views.py
+++ Manager/apks/views.py
@@ -1,5 +1,12 @@
 # -*- coding: utf-8 -*-
+from django.db import transaction
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.utils import timezone
+from django.core.urlresolvers import reverse
+from django.http import JsonResponse
+
+from models import Apps
 
 
 def index(request):
@@ -7,7 +14,25 @@ def index(request):
 
 
 def create(request):
+    return render(request, 'apks/create.html')
+
+
+def save(request):
     app_name = request.POST.get('inputAppName')
     app_version = request.POST.get('inputAppVersion')
-    # app_id =
-    return render(request, 'apks/create.html')
+    if Apps.objects.count():
+        app_id = Apps.objects.last().app_id
+    else:
+        app_id = 10000
+    with transaction.atomic():
+        Apps.objects.create(app_id=app_id, name=app_name, version=app_version, update_time=timezone.now())
+    return HttpResponseRedirect(reverse('apks:index'))
+
+
+def search(request):
+    data = []
+    for i in Apps.objects.all():
+        data.append(
+            {'id': i.id, 'app_id': i.app_id, 'name': i.name, 'version': i.version, 'update_time': i.update_time})
+    # data = Apps.objects.all()
+    return JsonResponse({'data': data})
