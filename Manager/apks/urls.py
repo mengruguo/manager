10548c2af4942f2c4dbffcaa40d914a5032a2e26
guomengru <guo.mengru@zte.com.cn>
2018-04-07 18:12:57 +0800

    增加编辑和删除功能

--- Manager/apks/urls.py
+++ Manager/apks/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     url(r'^$', index, name='index'),
     url(r'^create', create, name='create'),
     url(r'^save', save, name='save'),
-    url(r'^search', search, name='search')
+    url(r'^search', search, name='search'),
+    url(r'^edit', edit, name='edit'),
+    url(r'^delete', delete, name='delete')
 ]
--- Manager/apks/views.py
+++ Manager/apks/views.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
+from django.core.urlresolvers import reverse
 from django.db import transaction
 from django.http import HttpResponseRedirect
+from django.http import JsonResponse
 from django.shortcuts import render
 from django.utils import timezone
-from django.core.urlresolvers import reverse
-from django.http import JsonResponse
 
 from models import Apps
 
@@ -18,14 +18,21 @@ def create(request):
 
 
 def save(request):
+    apks_id = request.POST.get('id')
     app_name = request.POST.get('inputAppName')
     app_version = request.POST.get('inputAppVersion')
-    if Apps.objects.count():
-        app_id = Apps.objects.last().app_id + 1
+    if apks_id:
+        apk = Apps.objects.get(id=apks_id)
+        apk.name = app_name
+        apk.version = app_version
+        apk.save()
     else:
-        app_id = 10000
-    with transaction.atomic():
-        Apps.objects.create(app_id=app_id, name=app_name, version=app_version, update_time=timezone.now())
+        if Apps.objects.count():
+            app_id = Apps.objects.last().app_id + 1
+        else:
+            app_id = 10000
+        with transaction.atomic():
+            Apps.objects.create(app_id=app_id, name=app_name, version=app_version, update_time=timezone.now())
     return HttpResponseRedirect(reverse('apks:index'))
 
 
@@ -35,3 +42,18 @@ def search(request):
         data.append(
             {'id': i.id, 'app_id': i.app_id, 'name': i.name, 'version': i.version, 'update_time': i.update_time})
     return JsonResponse({'data': data})
+
+
+def edit(request):
+    try:
+        app = Apps.objects.get(id=request.GET.get('t', ''))
+    except Exception as e:
+        print e
+    else:
+        data = {'id': app.id, 'name': app.name, 'version': app.version}
+    return render(request, 'apks/create.html', {'data': data})
+
+
+def delete(request):
+    Apps.objects.get(id=request.GET.get('t')).delete()
+    return render(request, 'apks/index.html')
